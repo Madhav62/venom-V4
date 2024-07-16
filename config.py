e1cbdb72711bb005e9833700f7253d5088a84bc8
Madhav62 <17madhavs@gmail.com>
2024-07-16 16:35:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY VENOMxCRAZY
+#SCRIPT BY LORDXYLA
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6642853626:AAErv0Km2xX7IppRJwGeYaOmUHr7FsdiT-c")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("killer3005")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1730198714")
 
 
 
